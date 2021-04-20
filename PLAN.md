bc83b88986e14f1adc3d21cd84d3c44e8f97c9a9
Old Prego <spymasterluke@gmail.com>
2021-04-19 19:17:50 -0500

    Initial commit

---
+++ PLAN.md
@@ -0,0 +1,37 @@
+# Plans
+
+## Homepage
+
+- Button to go to tables
+- Button to go to Reservation
+- API Table link (straight JSON)
+- API Wait List (straight JSON)
+
+## View Tables
+
+- Dynamically create tables
+  - For loop through reservations
+    - First four go into current res
+    - Anything past that goes to waiting list
+- Home Page Button
+- Make Reservation Button
+- Clear Table
+- API Table Link
+- API Wait List
+
+## Make your Reservation
+
+- Home Page Button
+- View Tables Button
+- API Table Link
+- API Wait List
+- Form
+  - Built in HTML
+  - text inputs for:
+    - Name
+    - Phone
+    - Email
+    - Unique ID
+  - Submit Button
+    - Use POSTs
+    - Append to the view Tables array
---
+++ index.js
@@ -0,0 +1,29 @@
+const rButton = document.getElementById('rButton');
+
+rButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let rName = document.getElementById('rName').value.trim();
+    let rPhone = document.getElementById('rPhone').value.trim();
+    let rEmail = document.getElementById('rEmail').value.trim();
+    let rID = document.getElementById('rID').value.trim();
+
+    let newReservation = {
+        name: rName,
+        phone: parseInt(rPhone),
+        email: rEmail,
+        uniqID: rID,
+    };
+
+    fetch('/api/reservations', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newReservation),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+})
